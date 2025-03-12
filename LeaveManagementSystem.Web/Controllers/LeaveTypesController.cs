fc7f3504203c55ce6841d5543b94e737710c29a4
Jēkabs Andrejs Zvaigzne <47506475+juk-developer@users.noreply.github.com>
2025-03-12 10:07:08 +0200

    Setup and use AutoMapper

--- LeaveManagementSystem.Web/Controllers/LeaveTypesController.cs
+++ LeaveManagementSystem.Web/Controllers/LeaveTypesController.cs
@@ -7,29 +7,27 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using LeaveManagementSystem.Web.Data;
 using LeaveManagementSystem.Web.Models.LeaveTypes;
+using AutoMapper;
 
 namespace LeaveManagementSystem.Web.Controllers
 {
     public class LeaveTypesController : Controller
     {
         private readonly ApplicationDbContext _context;
+        private readonly IMapper _mapper;
 
-        public LeaveTypesController(ApplicationDbContext context)
+        public LeaveTypesController(ApplicationDbContext context, IMapper mapper)
         {
             _context = context;
+            _mapper = mapper;
         }
 
         // GET: LeaveTypes
         public async Task<IActionResult> Index()
         {
             var data = await _context.LeaveTypes.ToListAsync();
-            // convert the data model in view model
-            var viewData = data.Select(q => new IndexVM
-            {
-                Id = q.Id,
-                Name = q.Name,
-                Days = q.NumberOfDays
-            });
+            // convert the data model in view model - use auto mapper
+            var viewData = _mapper.Map<List<IndexVM>>(data);
             // return the view to the view
             return View(viewData);
         }
