fc339a9018da54a6d69688212a31782f703433ec
Jēkabs Andrejs Zvaigzne <47506475+juk-developer@users.noreply.github.com>
2025-03-12 11:53:41 +0200

    Setup base class and Change labels

--- LeaveManagementSystem.Web/Models/LeaveTypes/LeaveTypeCreateVM.cs
+++ LeaveManagementSystem.Web/Models/LeaveTypes/LeaveTypeCreateVM.cs
@@ -5,8 +5,11 @@ namespace LeaveManagementSystem.Web.Models.LeaveTypes
     public class LeaveTypeCreateVM
     {
         [Required]
+        [Length(4, 150, ErrorMessage = "Your name should be 4-150 length.")]
         public string Name { get; set; } = string.Empty;
         [Required]
+        [Range(1, 90)]
+        [Display(Name = "Maximum Allocation of Days")]
         public int NumberOfDays { get; set; }
     }
 }
--- LeaveManagementSystem.Web/Models/LeaveTypes/LeaveTypeEditVM.cs
+++ LeaveManagementSystem.Web/Models/LeaveTypes/LeaveTypeEditVM.cs
@@ -2,15 +2,14 @@
 
 namespace LeaveManagementSystem.Web.Models.LeaveTypes
 {
-    public class LeaveTypeEditVM
+    public class LeaveTypeEditVM : BaseLeaveTypeVM
     {
-        [Required]
-        public int Id { get; set; }
         [Required]
         [Length(4, 150, ErrorMessage = "Your name should be 4-150 length.")]
         public string Name { get; set; } = string.Empty;
         [Required]
         [Range(1, 90)]
+        [Display(Name = "Maximum Allocation of Days")]
         public int NumberOfDays { get; set; }
     }
 }
