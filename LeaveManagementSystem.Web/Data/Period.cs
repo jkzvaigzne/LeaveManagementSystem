f15e1452376330c6fb5e758d3a722c524cc27c11
Jēkabs Andrejs Zvaigzne <47506475+juk-developer@users.noreply.github.com>
2025-03-15 15:36:04 +0200

    Create Leave Allocation, BaseEntity, Period & Update LeaveType classes

---
+++ LeaveManagementSystem.Web/Data/Period.cs
@@ -0,0 +1,8 @@
+﻿namespace LeaveManagementSystem.Web.Data
+{
+    public class Period : BaseEntity
+    {
+        public DateOnly StartDate { get; set; }
+        public DateOnly EndDate { get; set; }
+    }
+}
