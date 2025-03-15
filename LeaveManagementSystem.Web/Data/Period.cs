2c5e8a981eef284ff0a057db51e44b4b78a06f59
Jēkabs Andrejs Zvaigzne <47506475+juk-developer@users.noreply.github.com>
2025-03-15 16:59:37 +0200

    Add Period Management Module and Update Controller Scaffolding & Migration

--- LeaveManagementSystem.Web/Data/Period.cs
+++ LeaveManagementSystem.Web/Data/Period.cs
@@ -2,6 +2,7 @@
 {
     public class Period : BaseEntity
     {
+        public string Name { get; set; }
         public DateOnly StartDate { get; set; }
         public DateOnly EndDate { get; set; }
     }
