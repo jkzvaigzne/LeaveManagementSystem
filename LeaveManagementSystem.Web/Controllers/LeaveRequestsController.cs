a079272b3f0cb5d6a9cdaf0b4790d4215c043053
Jēkabs Andrejs Zvaigzne <47506475+juk-developer@users.noreply.github.com>
2025-03-23 20:09:29 +0200

    Add Query String to Leave Request Navigation

--- LeaveManagementSystem.Web/Controllers/LeaveRequestsController.cs
+++ LeaveManagementSystem.Web/Controllers/LeaveRequestsController.cs
@@ -15,10 +15,10 @@ namespace LeaveManagementSystem.Web.Controllers
             return View(model);
         }
 
-        public async Task<IActionResult> Create()
+        public async Task<IActionResult> Create(int? leaveTypeId)
         {
             var leaveTypes = await _leaveTypesService.GetAll();
-            var leaveTypesList = new SelectList(leaveTypes, "Id","Name");
+            var leaveTypesList = new SelectList(leaveTypes, "Id","Name", leaveTypeId);
             var model = new LeaveRequestCreateVM
             {
                 StartDate = DateOnly.FromDateTime(DateTime.Now),
