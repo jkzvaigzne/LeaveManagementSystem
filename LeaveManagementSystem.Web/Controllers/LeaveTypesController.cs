c5dc550b3b719c8166e372e28da458545a79b5b8
Jēkabs Andrejs Zvaigzne <47506475+juk-developer@users.noreply.github.com>
2025-03-24 20:04:28 +0200

    Implement Logging to Seq and Testing

--- LeaveManagementSystem.Web/Controllers/LeaveTypesController.cs
+++ LeaveManagementSystem.Web/Controllers/LeaveTypesController.cs
@@ -5,13 +5,14 @@ using Microsoft.EntityFrameworkCore;
 namespace LeaveManagementSystem.Web.Controllers
 {
     [Authorize(Roles = Roles.Administrator)]
-    public class LeaveTypesController(ILeaveTypesService _leaveTypesService) : Controller
+    public class LeaveTypesController(ILeaveTypesService _leaveTypesService, ILogger<LeaveTypesController> _logger) : Controller
     {
         private const string NameExistsValidationMessage = "This leave type already exists in the database";
 
         // GET: LeaveTypes
         public async Task<IActionResult> Index()
         {
+            _logger.LogInformation("Getting all leave types");
             var viewData = await _leaveTypesService.GetAll();
             return View(viewData);
         }
@@ -55,6 +56,7 @@ namespace LeaveManagementSystem.Web.Controllers
                 await _leaveTypesService.Create(leaveTypeCreate);
                 return RedirectToAction(nameof(Index));
             }
+            _logger.LogWarning("Leave Types attempt failed");
             return View(leaveTypeCreate);
         }
 
